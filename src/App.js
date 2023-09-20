79643d89c459c515c9db4a080f7d722441e1397f
thang1911 <braveshinefateubw@gmail.com>
2023-09-20 14:41:35 +0700

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import Man from './Components/Man';
+import Main from './Components/Man';
+
+  const csvFiles = [
+    "Skchaunoi.csv",
+    "skchiatay.csv",
+    "skConLapGiaDinh.csv",
+    "skDudoancontuonglai.csv",
+    "skGapNhau.csv",
+    "skhanhphuc.csv",
+    "skkethon.csv",
+    "sklyhon.csv",
+    "skmaymua.csv",
+    "skmuasam.csv",
+    "skngoaitinh.csv",
+    "sknym.csv",
+    "skSinhConDauLong.csv",
+    "skSinhConThuHai.csv",
+    "skToTinh.csv",
+    "skTuoiGia.csv",
+    "skvohoacchongchettruoc.csv",
+  ];
 
 function App() {
   return (
     <div className="App">
-      <Man />
+      {csvFiles.map((file, index) => (
+        <div key={index}>
+          <Main file={file} />
+          <p>-----------------------</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Components/Man.jsx
+++ src/Components/Man.jsx
@@ -1,187 +1,46 @@
-import React, { useState } from "react";
-
-import {
-  useCSVReader,
-  lightenDarkenColor,
-  formatFileSize,
-} from "react-papaparse";
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
 import Table from "./Table";
 
-const GREY = "#CCC";
-const GREY_LIGHT = "rgba(255, 255, 255, 0.4)";
-const DEFAULT_REMOVE_HOVER_COLOR = "#A01919";
-const REMOVE_HOVER_COLOR_LIGHT = lightenDarkenColor(
-  DEFAULT_REMOVE_HOVER_COLOR,
-  40
-);
-const GREY_DIM = "#686868";
+function Main({ file }) {
+  const [csvData, setCsvData] = useState([]);
 
-const styles = {
-  zone: {
-    alignItems: "center",
-    border: `2px dashed ${GREY}`,
-    borderRadius: 20,
-    display: "flex",
-    flexDirection: "column",
-    height: "100%",
-    justifyContent: "center",
-    padding: 20,
-  },
-  file: {
-    background: "linear-gradient(to bottom, #EEE, #DDD)",
-    borderRadius: 20,
-    display: "flex",
-    height: 120,
-    width: 120,
-    position: "relative",
-    zIndex: 10,
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  info: {
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "column",
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  size: {
-    backgroundColor: GREY_LIGHT,
-    borderRadius: 3,
-    marginBottom: "0.5em",
-    justifyContent: "center",
-    display: "flex",
-  },
-  name: {
-    backgroundColor: GREY_LIGHT,
-    borderRadius: 3,
-    fontSize: 12,
-    marginBottom: "0.5em",
-  },
-  progressBar: {
-    bottom: 14,
-    position: "absolute",
-    width: "100%",
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  zoneHover: {
-    borderColor: GREY_DIM,
-  },
-  default: {
-    borderColor: GREY,
-  },
-  remove: {
-    height: 23,
-    position: "absolute",
-    right: 6,
-    top: 6,
-    width: 23,
-  },
-};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/csvFiles/${file}`);
+        if (response.ok) {
+          const textData = await response.text();
 
-const Man = () => {
-  const { CSVReader } = useCSVReader();
-  const [zoneHover, setZoneHover] = useState(false);
-  const [removeHoverColor, setRemoveHoverColor] = useState(
-    DEFAULT_REMOVE_HOVER_COLOR
-  );
-  const [data, setData] = useState()
-  const [uploadedFileName, setUploadedFileName] = useState(null);
+          const results = [];
+
+          Papa.parse(textData, {
+            header: true,
+            delimiter: ",",
+            complete: (result) => {
+              results.push(result.data);
+            },
+          });
+
+          console.log(results);
+          setCsvData(results);
+        } else {
+          console.error(`Error reading ${file}: HTTP ${response.status}`);
+        }
+      } catch (error) {
+        console.error(`Error reading ${file}:`, error);
+      }
+    };
+
+    fetchData();
+  }, [file]);
 
   return (
-    <>
-      <CSVReader
-        onUploadAccepted={(results, file) => {
-          console.log("---------------------------");
-          console.log(results);
-          console.log("---------------------------");
-          setZoneHover(false);
-          setUploadedFileName(file.name);
-          const [header, ...parsedData] = results.data;
-          const convertedData = parsedData.map((rowData) => {
-            return {
-              id: rowData[0],
-              mask: rowData[1],
-              thongtin: rowData[2],
-              image: rowData[3],
-              nu: rowData[4],
-              nam: rowData[5],
-              dotuoi: rowData[6],
-              danam: rowData[7],
-              danu: rowData[8],
-              ngswap: rowData[9],
-              vtrinam: rowData[10],
-              tomLuocText: rowData[11],
-            };
-          });
-          console.log(convertedData);
-          setData(convertedData);
-        }}
-        onDragOver={(event) => {
-          event.preventDefault();
-          setZoneHover(true);
-        }}
-        onDragLeave={(event) => {
-          event.preventDefault();
-          setZoneHover(false);
-        }}
-      >
-        {({
-          getRootProps,
-          acceptedFile,
-          ProgressBar,
-          getRemoveFileProps,
-          Remove,
-        }) => (
-          <>
-            <div
-              {...getRootProps()}
-              style={Object.assign(
-                {},
-                styles.zone,
-                zoneHover && styles.zoneHover
-              )}
-            >
-              {acceptedFile ? (
-                <>
-                  <div style={styles.file}>
-                    <div style={styles.info}>
-                      <span style={styles.size}>
-                        {formatFileSize(acceptedFile.size)}
-                      </span>
-                      <span style={styles.name}>{acceptedFile.name}</span>
-                    </div>
-                    <div style={styles.progressBar}>
-                      <ProgressBar />
-                    </div>
-                    <div
-                      {...getRemoveFileProps()}
-                      style={styles.remove}
-                      onMouseOver={(event) => {
-                        event.preventDefault();
-                        setRemoveHoverColor(REMOVE_HOVER_COLOR_LIGHT);
-                      }}
-                      onMouseOut={(event) => {
-                        event.preventDefault();
-                        setRemoveHoverColor(DEFAULT_REMOVE_HOVER_COLOR);
-                      }}
-                    >
-                      <Remove color={removeHoverColor} />
-                    </div>
-                  </div>
-                </>
-              ) : (
-                "Drop CSV file here or click to upload"
-              )}
-            </div>
-          </>
-        )}
-      </CSVReader>
-      {uploadedFileName && <p>Uploaded CSV file: {uploadedFileName}</p>}
-      <Table data={data} />
-    </>
+    <div>
+      <h3>Data from File {file}</h3>
+      <Table data={csvData[0]} />
+    </div>
   );
-};
+}
 
-export default Man;
+export default Main;
