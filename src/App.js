519684365e40eda09fd6e8521aaa81157edfa3e0
thang1911 <braveshinefateubw@gmail.com>
2023-09-20 14:49:46 +0700

    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './Components/Man';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Components/Man";
 
-  const csvFiles = [
-    "Skchaunoi.csv",
-    "skchiatay.csv",
-    "skConLapGiaDinh.csv",
-    "skDudoancontuonglai.csv",
-    "skGapNhau.csv",
-    "skhanhphuc.csv",
-    "skkethon.csv",
-    "sklyhon.csv",
-    "skmaymua.csv",
-    "skmuasam.csv",
-    "skngoaitinh.csv",
-    "sknym.csv",
-    "skSinhConDauLong.csv",
-    "skSinhConThuHai.csv",
-    "skToTinh.csv",
-    "skTuoiGia.csv",
-    "skvohoacchongchettruoc.csv",
-  ];
+const csvFiles = [
+  "Skchaunoi.csv",
+  "skchiatay.csv",
+  "skConLapGiaDinh.csv",
+  "skDudoancontuonglai.csv",
+  "skGapNhau.csv",
+  "skhanhphuc.csv",
+  "skkethon.csv",
+  "sklyhon.csv",
+  "skmaymua.csv",
+  "skmuasam.csv",
+  "skngoaitinh.csv",
+  "sknym.csv",
+  "skSinhConDauLong.csv",
+  "skSinhConThuHai.csv",
+  "skToTinh.csv",
+  "skTuoiGia.csv",
+  "skvohoacchongchettruoc.csv",
+];
 
 function App() {
   return (
-    <div className="App">
-      {csvFiles.map((file, index) => (
-        <div key={index}>
-          <Main file={file} />
-          <p>----------------------------</p>
+    <div>
+      {csvFiles && (
+        <div className="App">
+          {csvFiles.map((file, index) => (
+            <div key={index}>
+              <Main file={file} />
+              <p>----------------------------</p>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
